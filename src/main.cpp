c6ea33336addc9928c789535162ffb09d1dc2bd0
Valkyrie-freja <yadokame3.14@gmail.com>
2023-05-05 03:17:35 +0900

    light led with nn

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,30 @@
 #include "neural_network.h"
 #include "ShiftRegister.h"
 
-neural_network neural_network;
+neural_network nn;
+ShiftRegister shift_register(2, 3, 4);
+
+long time_before_ms;
 
 void setup() {
-  
+  nn.set_all_neuron_dt(0.001);
+  shift_register.setup();
+  time_before_ms = millis();
+  Serial.begin(9600);
 }
 
 void loop() {
+  // set delta t
+  int delta_t_ms = millis() - time_before_ms;
+  time_before_ms = millis();
+  nn.set_all_neuron_dt(0.001);
+  Serial.println(delta_t_ms);
+
+  // compute
+  nn.compute_neural_network();
   
+  // light spike
+  byte spike_bit_row = (byte)(nn.get_all_spike_bit());
+  shift_register.setLedRow(spike_bit_row);
+  delay(100);
 }
